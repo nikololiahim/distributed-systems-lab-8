19d13e295c86f823c1bd571a112856d69e4d511f
nikololiahim <olomishcak@outlook.com>
2020-09-28 20:01:53 +0300

    initial commit

--- logical_clock.py
+++ logical_clock.py
@@ -1,41 +1,41 @@
 from multiprocessing import Process, Pipe
-from os import getpid
-from datetime import datetime
 
 
+# Get the current local time of the process defined by pid
 def local_time(pid, counter):
     process_names = 'abc'
     return f'Process {process_names[pid]} {counter}'
 
 
+# Calculate the timestamp upon receiving a message
 def calc_recv_timestamp(recv_time_stamp, counter):
     for i in range(len(counter)):
         counter[i] = max(recv_time_stamp[i], counter[i])
     return counter
 
 
+# Event that does not concern other processes
 def event(pid, counter):
     counter[pid] += 1
-    # print('Something happened in {}!'. \
-    #       format(pid) + local_time(counter))
     return counter
 
 
+# Event that corresponds to sending a message
 def send_message(pipe, pid, counter):
     counter[pid] += 1
     pipe.send(('Empty shell', counter))
-    # print('Message sent from ' + str(pid) + local_time(counter))
     return counter
 
 
+# Event that corresponds to receiving a message
 def recv_message(pipe, pid, counter):
     message, timestamp = pipe.recv()
     counter[pid] += 1
     counter = calc_recv_timestamp(timestamp, counter)
-    # print('Message received at ' + str(pid) + local_time(counter))
     return counter
 
 
+# Sequence of events for process 1 (as per diagram)
 def process_one(pipe12):
     pid = 0
     counter = [0, 0, 0]
@@ -51,6 +51,7 @@ def process_one(pipe12):
     print(local_time(pid, counter))
 
 
+# Sequence of events for process 2 (as per diagram)
 def process_two(pipe21, pipe23):
     pid = 1
     counter = [0, 0, 0]
@@ -67,7 +68,7 @@ def process_two(pipe21, pipe23):
     print(local_time(pid, counter))
 
 
-
+# Sequence of events for process 3 (as per diagram)
 def process_three(pipe32):
     pid = 2
     counter = [0, 0, 0]
@@ -80,10 +81,14 @@ def process_three(pipe32):
     print(local_time(pid, counter))
 
 
+# Driver code
 if __name__ == '__main__':
+
+    # Creating 2 pipes
     _pipe12, _pipe21 = Pipe()
     _pipe23, _pipe32 = Pipe()
 
+    # Creating 3 processes connected by pipes
     process1 = Process(target=process_one,
                        args=(_pipe12,))
     process2 = Process(target=process_two,
